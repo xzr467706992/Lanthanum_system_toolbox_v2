5f8e7312a4c559e28a62e56142265f47b12a584f
LibXZR <xzr467706992@163.com>
2018-09-07 21:47:14 +0800

    Prevent patch ify

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     Lanthanum prevent patch    "
   ui_print "*******************************"
 }
 
@@ -63,6 +63,7 @@ REPLACE="
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/framework/services.jar
 "
 
 ##########################################################################################
